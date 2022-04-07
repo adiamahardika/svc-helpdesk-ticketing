0a5e92308be1c50e7d88ed7dd91591657624c289
Adia Mahardika <adiamahardika.work@gmail.com>
2022-04-07 09:08:45 +0700

    Add color response in get ticket status

--- entity/ticketStatus.go
+++ entity/ticketStatus.go
@@ -3,6 +3,7 @@ package entity
 type TicketStatus struct {
 	Id        int    `json:"id" gorm:"primaryKey"`
 	Name      string `json:"name"`
+	Color     string `json:"color"`
 	Index     int    `json:"index"`
 	CreatedAt string `json:"createdAt"`
 	UpdatedAt string `json:"updatedAt"`
