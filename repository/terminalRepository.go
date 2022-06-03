a612c10529f85f6907a1743cacf059633f3afc85
Adia Mahardika <adiamahardika.work@gmail.com>
2022-06-03 16:45:39 +0700

    Edit get terminal request

--- repository/terminalRepository.go
+++ repository/terminalRepository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"svc-myg-ticketing/entity"
 	"svc-myg-ticketing/model"
 )
@@ -10,15 +11,34 @@ type TerminalRepositoryInterface interface {
 }
 
 func (repo *repository) GetTerminal(request model.GetTerminalRequest) ([]entity.MsTerminal, error) {
-	var terminal []entity.MsTerminal
+	var result []entity.MsTerminal
+	var area_code string
+	var regional string
+	var grapari_id string
+	var terminal string
 
-	error := repo.db.Raw("SELECT ms_terminal.*, grapari_has_terminal.grapari_id AS grapari_id FROM ms_terminal LEFT OUTER JOIN grapari_has_terminal ON (ms_terminal.terminal_id = grapari_has_terminal.terminal_id)WHERE ms_terminal.terminal_id LIKE @TerminalId AND grapari_has_terminal.grapari_id LIKE @GrapariId AND area LIKE @AreaCode AND regional LIKE @Regional AND status LIKE @Status", model.GetTerminalRequest{
-		TerminalId: "%" + request.TerminalId + "%",
-		GrapariId:  "%" + request.GrapariId + "%",
-		AreaCode:   "%" + request.AreaCode + "%",
-		Regional:   "%" + request.Regional + "%",
+	if len(request.AreaCode) > 0 {
+		area_code = "area IN @AreaCode AND"
+	}
+	if len(request.Regional) > 0 {
+		regional = "regional IN @Regional AND"
+	}
+	if len(request.GrapariId) > 0 {
+		grapari_id = "grapari_has_terminal.grapari_id IN @GrapariId AND "
+	}
+	if len(request.TerminalId) > 0 {
+		terminal = "ms_terminal.terminal_id IN @TerminalId AND "
+	}
+
+	query := fmt.Sprintf("SELECT ms_terminal.*, grapari_has_terminal.grapari_id AS grapari_id FROM ms_terminal LEFT OUTER JOIN grapari_has_terminal ON (ms_terminal.terminal_id = grapari_has_terminal.terminal_id)WHERE %s %s %s %s status LIKE @Status", area_code, regional, grapari_id, terminal)
+
+	error := repo.db.Raw(query, model.GetTerminalRequest{
+		TerminalId: request.TerminalId,
+		GrapariId:  request.GrapariId,
+		AreaCode:   request.AreaCode,
+		Regional:   request.Regional,
 		Status:     "%" + request.Status + "%",
-	}).Find(&terminal).Error
+	}).Find(&result).Error
 
-	return terminal, error
+	return result, error
 }
