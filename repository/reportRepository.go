5b4e0161d222587bcb8d3a6c6b168fc03c346acb
Adia Mahardika <adiamahardika.work@gmail.com>
2022-06-06 16:36:29 +0700

    Edit get report response

--- repository/reportRepository.go
+++ repository/reportRepository.go
@@ -26,7 +26,7 @@ func (repo *repository) GetReport(request model.GetReportRequest) ([]entity.Tick
 	} else {
 		created_by = "AND username_pembuat IN @UsernamePembuat"
 	}
-	query = fmt.Sprintf("SELECT DISTINCT ON (ticket_code) * FROM (SELECT ticket.*, category.name AS category, ticket_isi.isi FROM ticket LEFT OUTER JOIN category ON (ticket.category = CAST(category.id AS varchar(10))) LEFT OUTER JOIN ticket_isi ON (ticket.ticket_code = ticket_isi.ticket_code) WHERE prioritas IN @Priority AND status IN @Status AND assigned_to LIKE @AssignedTo %s %s AND ticket.tgl_dibuat >= @StartDate AND ticket.tgl_dibuat <= @EndDate ORDER BY ticket_isi.tgl_dibuat ASC) AS ticket", category, created_by)
+	query = fmt.Sprintf("SELECT DISTINCT ON (ticket_code) * FROM (SELECT ticket.*, category.name AS category, ticket_isi.isi, ms_area.area_name, ms_grapari.name AS grapari_name FROM ticket LEFT OUTER JOIN category ON (ticket.category = CAST(category.id AS varchar(10))) LEFT OUTER JOIN ticket_isi ON (ticket.ticket_code = ticket_isi.ticket_code) LEFT OUTER JOIN ms_area ON (ticket.area_code = ms_area.area_code) LEFT OUTER JOIN ms_grapari ON (ticket.grapari_id = ms_grapari.grapari_id) WHERE prioritas IN @Priority AND ticket.status IN @Status AND assigned_to LIKE @AssignedTo %s %s AND ticket.tgl_dibuat >= @StartDate AND ticket.tgl_dibuat <= @EndDate ORDER BY ticket_isi.tgl_dibuat ASC) AS ticket", category, created_by)
 
 	error := repo.db.Raw(query, model.GetReportRequest{
 		AssignedTo:      "%" + request.AssignedTo + "%",
