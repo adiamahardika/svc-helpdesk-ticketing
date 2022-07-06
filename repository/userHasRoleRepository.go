23d6f2a120195b62295e8d1981edc86609285501
Adia Mahardika <adiamahardika.work@gmail.com>
2022-07-06 10:17:30 +0700

    Edit query in all user service

--- repository/userHasRoleRepository.go
+++ repository/userHasRoleRepository.go
@@ -11,7 +11,7 @@ func (repo *repository) CreateUserHasRole(id_user int, id_role int) error {
 
 	var user_has_role entity.UserHasRole
 
-	error := repo.db.Raw("INSERT INTO ticketing_user_has_role(id_user, id_role) VALUES(?, ?) RETURNING ticketing_user_has_role.*", id_user, id_role).Find(&user_has_role).Error
+	error := repo.db.Raw("INSERT INTO model_has_roles(id_user, id_role) VALUES(?, ?) RETURNING model_has_roles.*", id_user, id_role).Find(&user_has_role).Error
 
 	return error
 }
@@ -20,7 +20,7 @@ func (repo *repository) DeleteUserHasRole(id_user int) error {
 
 	var user_has_role entity.UserHasRole
 
-	error := repo.db.Raw("DELETE FROM ticketing_user_has_role WHERE id_user = ? RETURNING ticketing_user_has_role.*", id_user).Find(&user_has_role).Error
+	error := repo.db.Raw("DELETE FROM model_has_roles WHERE id_user = ? RETURNING model_has_roles.*", id_user).Find(&user_has_role).Error
 
 	return error
 }
