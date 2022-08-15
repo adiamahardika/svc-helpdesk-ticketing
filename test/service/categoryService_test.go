f40eb371735ccac6c25bf82a29b3bd72a9366862
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-15 14:00:18 +0700

    Create get detail category test

--- test/service/categoryService_test.go
+++ test/service/categoryService_test.go
@@ -313,3 +313,70 @@ func TestDeleteCategoryService(t *testing.T) {
 		})
 	}
 }
+
+func TestGetDetailCategoryService(t *testing.T) {
+
+	date, _ := time.Parse("0001-01-01T00:00:00Z", time.RFC1123Z)
+	tests := []struct {
+		name              string
+		request           string
+		expectedReturn    []model.CreateCategoryRequest
+		expectedTotalPage float64
+		expectedError     error
+	}{{
+		name:    "Success Get Detail Category",
+		request: "78",
+		expectedReturn: []model.CreateCategoryRequest{
+			{
+				Id:   78,
+				Name: "Bill Acceptor Rupiah",
+				SubCategory: []entity.SubCategory{{
+					Id:         40,
+					Name:       "Tidak dapat menerima uang/uang tereject",
+					IdCategory: 78,
+					Priority:   "High",
+					CreatedAt:  date,
+					UpdatedAt:  date,
+				},
+					{
+						Id:         41,
+						Name:       "Mati/error",
+						IdCategory: 78,
+						Priority:   "Critical",
+						CreatedAt:  date,
+						UpdatedAt:  date,
+					},
+					{
+						Id:         42,
+						Name:       "Uang tersangkut di dalam Bill acceptor",
+						IdCategory: 78,
+						Priority:   "High",
+						CreatedAt:  date,
+						UpdatedAt:  date,
+					}},
+				IsActive: "true",
+				UpdateAt: date,
+			},
+		},
+		expectedTotalPage: 1,
+		expectedError:     nil,
+	}}
+
+	for _, test := range tests {
+		categoryRepository.Mock.On("GetDetailCategory", &test.request).Return([]entity.Category{
+			{
+				Id:          78,
+				Name:        "Bill Acceptor Rupiah",
+				SubCategory: `[{"id":40,"name":"Tidak dapat menerima uang/uang tereject","idCategory":78,"priority":"High","createdAt":"0001-01-01T00:00:00Z","updatedAt":"0001-01-01T00:00:00Z"},{"id":41,"name":"Mati/error","idCategory":78,"priority":"Critical","createdAt":"0001-01-01T00:00:00Z","updatedAt":"0001-01-01T00:00:00Z"},{"id":42,"name":"Uang tersangkut di dalam Bill acceptor","idCategory":78,"priority":"High","createdAt":"0001-01-01T00:00:00Z","updatedAt":"0001-01-01T00:00:00Z"}]`,
+				IsActive:    "true",
+				UpdateAt:    date,
+			},
+		}, nil)
+
+		t.Run(test.name, func(t *testing.T) {
+			response, error := categoryService.GetDetailCategory(&test.request)
+			require.Equal(t, test.expectedReturn, response)
+			require.Equal(t, test.expectedError, error)
+		})
+	}
+}
