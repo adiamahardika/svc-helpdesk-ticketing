34da31affac01c9f7b70a2d1b1e603a82f300efa
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-16 10:15:09 +0700

    Create get email notif service test

--- test/service/emailNotifService_test.go
+++ test/service/emailNotifService_test.go
@@ -14,7 +14,7 @@ import (
 var emailNotifRepository = &repositoryMock.RepositoryMock{Mock: mock.Mock{}}
 var emailNotifService = service.EmailNotifService(emailNotifRepository)
 
-func TestCreateEmailNotif(t *testing.T) {
+func Test_Service_EmailNotif_Create(t *testing.T) {
 	date := time.Now()
 	tests := []struct {
 		name           string
@@ -42,6 +42,42 @@ func TestCreateEmailNotif(t *testing.T) {
 
 		t.Run(test.name, func(t *testing.T) {
 			result, error := emailNotifService.CreateEmailNotif(test.request)
+			require.Equal(t, test.expectedReturn, result)
+			require.Equal(t, test.expectedError, error)
+		})
+	}
+}
+
+func Test_Service_EmailNotif_Get(t *testing.T) {
+	date := time.Now()
+	tests := []struct {
+		name           string
+		expectedReturn []entity.EmailNotif
+		expectedError  error
+	}{{
+		name: "Success",
+		expectedReturn: []entity.EmailNotif{
+			{
+				Id:        1,
+				Email:     "dev@mail.com",
+				CreatedAt: date,
+				UpdatedAt: date,
+			},
+			{
+				Id:        2,
+				Email:     "dev2@gmail.com",
+				CreatedAt: date,
+				UpdatedAt: date,
+			},
+		},
+		expectedError: nil,
+	}}
+
+	for _, test := range tests {
+		emailNotifRepository.Mock.On("GetEmailNotif").Return(test.expectedReturn, test.expectedError)
+
+		t.Run(test.name, func(t *testing.T) {
+			result, error := emailNotifService.GetEmailNotif()
 			require.Equal(t, test.expectedReturn, result)
 			require.Equal(t, test.expectedError, error)
 		})
