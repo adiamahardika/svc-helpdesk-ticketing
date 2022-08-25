6eeb51b36b5b2556d62bc865b55a8b0b6b152539
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-25 22:26:54 +0700

    Create get detail category repo test

--- test/repository/categoryRepostory_test.go
+++ test/repository/categoryRepostory_test.go
@@ -240,3 +240,45 @@ func Test_Repo_Category_Delete(t *testing.T) {
 		})
 	}
 }
+
+func Test_Repo_Category_GetDetail(t *testing.T) {
+
+	gormDB, mock := dbmock.DbMock(t)
+
+	repository := repository.Repository(gormDB)
+
+	date := time.Time{}
+	tests := []struct {
+		name           string
+		request        string
+		expectedQuery  string
+		expectedReturn []entity.Category
+	}{{
+		name:          "Success",
+		request:       "3",
+		expectedQuery: "SELECT ticketing_category.*, JSON_AGG(JSON_BUILD_OBJECT('id', ticketing_sub_category.id, 'idCategory', ticketing_sub_category.id_category, 'name', ticketing_sub_category.name, 'priority', ticketing_sub_category.priority)) AS sub_category FROM ticketing_category LEFT OUTER JOIN ticketing_sub_category ON (ticketing_category.id = ticketing_sub_category.id_category) WHERE ticketing_category.id = $1 GROUP BY ticketing_category.id",
+		expectedReturn: []entity.Category{
+			{
+				Id:          3,
+				Name:        "Test Category",
+				SubCategory: "list_of_sub_category",
+				IsActive:    "False",
+				UpdateAt:    date,
+			},
+		},
+	}}
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+			category := sqlmock.NewRows([]string{"id", "name", "sub_category", "is_active", "updated_at"}).AddRow(test.expectedReturn[0].Id, test.expectedReturn[0].Name, test.expectedReturn[0].SubCategory, test.expectedReturn[0].IsActive, test.expectedReturn[0].UpdateAt)
+
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
+
+			res, err := repository.GetDetailCategory(&test.request)
+
+			require.NoError(t, err)
+			require.Equal(t, test.expectedReturn, res)
+		})
+	}
+}
