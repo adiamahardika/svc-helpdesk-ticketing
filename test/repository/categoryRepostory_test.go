8f5e104b4c4123cf9286579082d872be35061233
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-25 20:54:33 +0700

    Create get catgory repo test

---
+++ test/repository/categoryRepostory_test.go
@@ -0,0 +1,78 @@
+package repository_test
+
+import (
+	"math"
+	"regexp"
+	"svc-myg-ticketing/entity"
+	dbmock "svc-myg-ticketing/mock/db_mock"
+	"svc-myg-ticketing/model"
+	"svc-myg-ticketing/repository"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Repo_Category_Get(t *testing.T) {
+
+	gormDB, mock := dbmock.DbMock(t)
+
+	repository := repository.Repository(gormDB)
+
+	date := time.Time{}
+	tests := []struct {
+		name           string
+		request        *model.GetCategoryRequest
+		expectedQuery  string
+		expectedReturn []entity.Category
+	}{{
+		name: "Get All",
+		request: &model.GetCategoryRequest{
+			Size:       math.MaxInt16,
+			StartIndex: 0,
+			IsActive:   "",
+		},
+		expectedQuery: "SELECT ticketing_category.*, JSON_AGG(JSON_BUILD_OBJECT('id', ticketing_sub_category.id, 'idCategory', ticketing_sub_category.id_category, 'name', ticketing_sub_category.name, 'priority', ticketing_sub_category.priority)) AS sub_category FROM ticketing_category LEFT OUTER JOIN ticketing_sub_category ON (ticketing_category.id = ticketing_sub_category.id_category) WHERE is_active LIKE $1 GROUP BY ticketing_category.id ORDER BY name ASC LIMIT $2 OFFSET $3",
+		expectedReturn: []entity.Category{
+			{
+				Id:          3,
+				Name:        "Test Category",
+				SubCategory: "list_of_sub_category",
+				IsActive:    "False",
+				UpdateAt:    date,
+			},
+		},
+	}, {
+		name: "Active Category",
+		request: &model.GetCategoryRequest{
+			Size:       100,
+			StartIndex: 0,
+			IsActive:   "true",
+		},
+		expectedQuery: "SELECT ticketing_category.*, JSON_AGG(JSON_BUILD_OBJECT('id', ticketing_sub_category.id, 'idCategory', ticketing_sub_category.id_category, 'name', ticketing_sub_category.name, 'priority', ticketing_sub_category.priority)) AS sub_category FROM ticketing_category LEFT OUTER JOIN ticketing_sub_category ON (ticketing_category.id = ticketing_sub_category.id_category) WHERE is_active LIKE $1 GROUP BY ticketing_category.id ORDER BY name ASC LIMIT $2 OFFSET $3",
+		expectedReturn: []entity.Category{
+			{
+				Id:          3,
+				Name:        "Test Category",
+				SubCategory: "list_of_sub_category",
+				IsActive:    "True",
+				UpdateAt:    date,
+			},
+		},
+	}}
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+			area := sqlmock.NewRows([]string{"id", "name", "sub_category", "is_active", "updated_at"}).AddRow(test.expectedReturn[0].Id, test.expectedReturn[0].Name, test.expectedReturn[0].SubCategory, test.expectedReturn[0].IsActive, test.expectedReturn[0].UpdateAt)
+
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(area)
+
+			res, err := repository.GetCategory(test.request)
+
+			require.NoError(t, err)
+			require.Equal(t, test.expectedReturn, res)
+		})
+	}
+}
