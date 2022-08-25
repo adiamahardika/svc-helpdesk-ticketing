976ae8b8363efb5ca552f8551f9433e4045470de
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-25 22:24:14 +0700

    Create delete category repo test

--- test/repository/categoryRepostory_test.go
+++ test/repository/categoryRepostory_test.go
@@ -65,9 +65,9 @@ func Test_Repo_Category_Get(t *testing.T) {
 	for _, test := range tests {
 
 		t.Run(test.name, func(t *testing.T) {
-			area := sqlmock.NewRows([]string{"id", "name", "sub_category", "is_active", "updated_at"}).AddRow(test.expectedReturn[0].Id, test.expectedReturn[0].Name, test.expectedReturn[0].SubCategory, test.expectedReturn[0].IsActive, test.expectedReturn[0].UpdateAt)
+			category := sqlmock.NewRows([]string{"id", "name", "sub_category", "is_active", "updated_at"}).AddRow(test.expectedReturn[0].Id, test.expectedReturn[0].Name, test.expectedReturn[0].SubCategory, test.expectedReturn[0].IsActive, test.expectedReturn[0].UpdateAt)
 
-			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(area)
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
 
 			res, err := repository.GetCategory(test.request)
 
@@ -109,9 +109,9 @@ func Test_Repo_Category_Count(t *testing.T) {
 	for _, test := range tests {
 
 		t.Run(test.name, func(t *testing.T) {
-			area := sqlmock.NewRows([]string{"total_data"}).AddRow(test.expectedReturn)
+			category := sqlmock.NewRows([]string{"total_data"}).AddRow(test.expectedReturn)
 
-			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(area)
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
 
 			res, err := repository.CountCategory(test.request)
 
@@ -151,9 +151,9 @@ func Test_Repo_Category_Create(t *testing.T) {
 	for _, test := range tests {
 
 		t.Run(test.name, func(t *testing.T) {
-			area := sqlmock.NewRows([]string{"id", "name", "is_active", "update_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Name, test.expectedReturn.IsActive, test.expectedReturn.UpdateAt)
+			category := sqlmock.NewRows([]string{"id", "name", "is_active", "update_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Name, test.expectedReturn.IsActive, test.expectedReturn.UpdateAt)
 
-			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(area)
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
 
 			res, err := repository.CreateCategory(test.request)
 
@@ -192,14 +192,51 @@ func Test_Repo_Category_Update(t *testing.T) {
 	for _, test := range tests {
 
 		t.Run(test.name, func(t *testing.T) {
-			area := sqlmock.NewRows([]string{"id", "name", "is_active", "update_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Name, test.expectedReturn.IsActive, test.expectedReturn.UpdateAt)
+			category := sqlmock.NewRows([]string{"id", "name", "is_active", "update_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Name, test.expectedReturn.IsActive, test.expectedReturn.UpdateAt)
 
-			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(area)
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
 
 			res, err := repository.UpdateCategory(test.request)
 
 			require.NoError(t, err)
 			require.Equal(t, test.expectedReturn, res)
+		})
+	}
+}
+
+func Test_Repo_Category_Delete(t *testing.T) {
+
+	gormDB, mock := dbmock.DbMock(t)
+
+	repository := repository.Repository(gormDB)
+
+	tests := []struct {
+		name           string
+		request        int
+		expectedQuery  string
+		expectedReturn model.CreateCategoryRequest
+	}{{
+		name:          "Success",
+		request:       1,
+		expectedQuery: "UPDATE ticketing_category SET is_active = $1 WHERE id = $2 RETURNING ticketing_category.*",
+		expectedReturn: model.CreateCategoryRequest{
+			Id:       1,
+			Name:     "Test Category",
+			IsActive: "true",
+			UpdateAt: time.Time{},
+		},
+	}}
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+			category := sqlmock.NewRows([]string{"id", "name", "is_active", "update_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Name, test.expectedReturn.IsActive, test.expectedReturn.UpdateAt)
+
+			mock.ExpectQuery(regexp.QuoteMeta(test.expectedQuery)).WillReturnRows(category)
+
+			err := repository.DeleteCategory(&test.request)
+
+			require.NoError(t, err)
 		})
 	}
 }
