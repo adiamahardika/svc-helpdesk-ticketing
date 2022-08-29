c3c6a6ed9d9003eaf85b19e6a4c806474cd2f23f
Adia Mahardika <adiamahardika.work@gmail.com>
2022-08-29 16:15:47 +0700

    Create update email notif repo test

--- test/repository/emailNotifRepository_test.go
+++ test/repository/emailNotifRepository_test.go
@@ -50,3 +50,47 @@ func Test_Repo_EmailNotif_Get(t *testing.T) {
 		})
 	}
 }
+
+func Test_Repo_EmailNotif_Update(t *testing.T) {
+
+	gormDB, mock := dbmock.DbMock(t)
+
+	repository := repository.Repository(gormDB)
+
+	tests := []struct {
+		name           string
+		request        *entity.EmailNotif
+		expectedQuery  string
+		expectedReturn entity.EmailNotif
+	}{{
+		name: "Success",
+		request: &entity.EmailNotif{
+			Id:        1,
+			Email:     "test@mail.com",
+			UpdatedAt: time.Time{},
+		},
+		expectedQuery: "UPDATE ticketing_email_notif SET email = $1, updated_at = $2 WHERE id = $3 RETURNING ticketing_email_notif.*",
+		expectedReturn: entity.EmailNotif{
+			Id:        1,
+			Email:     "test@mail.com",
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+		},
+	}}
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+			sub_category := sqlmock.NewRows([]string{"id", "email", "created_at", "updated_at"}).AddRow(test.expectedReturn.Id, test.expectedReturn.Email, test.expectedReturn.CreatedAt, test.expectedReturn.UpdatedAt)
+
+			mock.ExpectQuery(regexp.QuoteMeta(
+				test.expectedQuery)).
+				WillReturnRows(sub_category)
+
+			res, err := repository.UpdateEmailNotif(test.request)
+
+			require.NoError(t, err)
+			require.Equal(t, test.expectedReturn, res)
+		})
+	}
+}
